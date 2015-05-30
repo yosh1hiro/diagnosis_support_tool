97cf82bb19bc1b9da7114936847a7dfbd9c5279a
yoshihiro asada <yoshihiro0921@gmail.com>
2015-05-30 23:20:46 +0900

    Implement cheif_complaint and disease

--- sample/diagnosis.rb
+++ sample/diagnosis.rb
@@ -1,3 +1,14 @@
 class Diagnosis
+  require './chief_complaint'
+  require './disease'
+  include ChiefComplaint
+  include Disease
+
+  def chose_cheif_complaint
+    puts "問診をはじめます"
+    display_cheif_complaint()
+  end
 
 end
+
+puts Diagnosis.new.chose_cheif_complaint
--- sample/disease.rb
+++ sample/disease.rb
@@ -1,3 +1,8 @@
 module Disease
-
+  def display_disease()
+    puts "調べたい疾患を選択してください"
+    puts "1. 虫垂炎, 2. 胆嚢炎, 3.腸閉塞, 4.尿路結石"
+    d = gets.chomp.to_i
+    puts "okay!"
+  end
 end
