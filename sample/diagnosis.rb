76ff03d118e38609c52e5bf286f6efa694a4f561
yoshihiro asada <yoshihiro0921@gmail.com>
2015-05-31 01:14:49 +0900

    Complete sample

--- sample/diagnosis.rb
+++ sample/diagnosis.rb
@@ -1,9 +1,16 @@
 class Diagnosis
   require './chief_complaint'
   require './disease'
+  require './history_talking'
+  require './test_result'
   include ChiefComplaint
   include Disease
+  include HistoryTalking
+  include TestResult
 
+  @post_odds = 0
+  @pre_odds = 0
+ 
   def chose_cheif_complaint
     puts "問診をはじめます"
     display_cheif_complaint()
--- sample/disease.rb
+++ sample/disease.rb
@@ -3,6 +3,18 @@ module Disease
     puts "調べたい疾患を選択してください"
     puts "1. 虫垂炎, 2. 胆嚢炎, 3.腸閉塞, 4.尿路結石"
     d = gets.chomp.to_i
-    puts "okay!"
+    case d
+    when 1
+      @pre_odds = 0.17 / (1 - 0.17)
+      medical_history()
+    when 2
+      puts "本で調べてね!"
+    when 3
+      puts "本で調べてね!"
+    when 4
+      puts "痛いよね。。。"
+    else
+      display_disease()
+    end
   end
 end
