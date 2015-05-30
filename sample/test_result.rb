1c0f674f8c24c645407c145de6d731b2d68eddbc
yoshihiro asada <yoshihiro0921@gmail.com>
2015-05-31 00:09:40 +0900

    Implemant to_odds and to_probability

--- sample/test_result.rb
+++ sample/test_result.rb
@@ -1,3 +1,12 @@
 module TestResult
+  def to_odds(p)
+    odds = p / (1 - p)
+    puts odds
+  end
 
+  def to_probability(o)
+    probability = o / (o + 1)
+    puts "#{probability * 100} %"
+
+  end
 end
